84c24e4598d2c548ba4e58a31d2445885e803b79
Fritz Salar <fritz.salar@zuhlke.com>
2021-01-23 05:28:16 +0800

    Initial react native tv app

---
+++ ios/Pods/Target Support Files/Pods-TVapp-TVappTests/Pods-TVapp-TVappTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TVapp_TVappTests : NSObject
+@end
+@implementation PodsDummy_Pods_TVapp_TVappTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-TVapp-tvOS-TVapp-tvOSTests/Pods-TVapp-tvOS-TVapp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TVapp_tvOS_TVapp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_TVapp_tvOS_TVapp_tvOSTests
+@end
