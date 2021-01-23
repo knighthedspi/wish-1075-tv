a8c642adfad274ac7fc760afb87410d3e6575be9
Fritz Salar <fritz.salar@zuhlke.com>
2021-01-23 18:41:20 +0800

    React Native typescript

---
+++ Wish1075/android/app/src/main/java/com/wish1075/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wish1075;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Wish1075";
+  }
+}
